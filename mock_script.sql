7e52926e2d74c9de37625d0964758f0eb859783d
jezv <jezv@yandex.ru>
2025-02-23 12:53:40 +0800

    task4

--- mock_script.sql
+++ mock_script.sql
@@ -3,20 +3,27 @@ declare
 	userid uuid;
 	inputid uuid;
 	names_ text[] := array['Воловиков Георгий Александрович', 'Какой-то нибудь ещё чёл'];
-	var_name text[];
+	var_name text;
 begin
 
-foreach var_name slice 1 in array names_ loop
+foreach var_name in array names_ loop
 	userid := gen_random_uuid();
 	INSERT INTO gosha.users VALUES (userid, 
 	(SELECT id FROM gosha.posts ORDER BY random() LIMIT 1), 
 	var_name, round(random()*100));
 	for input_index in 0..100 loop
 		inputid := gen_random_uuid();
-		INSERT INTO gosha.inputs VALUES (inputid, random()*100, (random()-0.5)*100, 750+(random()*20), random()*50, random(), 300+random()*200);
+		INSERT INTO gosha.inputs VALUES (inputid, random()*100, (random()-0.5)*150, 750+(random()*20), random()*50, random(), 300+random()*250);
 		INSERT INTO gosha.history (user_id, input_id, logitude, latitude, device_id) 
 		VALUES (userid,inputid,random()*50,random()*50,(SELECT id FROM gosha.devices ORDER BY random() LIMIT 1));
 	end loop;
 end loop;
 
 end $$;
+
+select t3.fullname, t4.name, count(*), sum(gosha."fnInputValidate"(t2.height, t2.temperature, t2.pressure, t2.wind_speed, t2.wind_direction)::integer) 
+from gosha.history as t1
+inner join gosha.inputs as t2 on t1.input_id = t2.id
+inner join gosha.users as t3 on t1.user_id = t3.id
+inner join  gosha.posts as t4 on t3.post = t4.id
+group by t3.fullname, t4.name;
